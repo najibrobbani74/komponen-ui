8f60a1450bc233046795512b9f4f6105737b50c1
Muhammad Najib Robbani <najibrobbani74@gmail.com>
2024-09-24 13:48:14 +0700

    [CONFIG] config deployment setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://najibrobbani74.github.io',
+  base: 'komponen-ui',
   integrations: [tailwind()]
 });
